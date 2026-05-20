b62ddb0eb32565c6b09c04e81ac5cc0d79aaf268
walker666777888 <walker666777888@gmail.com>
2026-05-20 11:41:28 +0530

    fix: redirect root to login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,3 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
-</script>
-
 <svelte:head>
-	<title>Tari Policy Quiz</title>
+	<title>CompliancePro</title>
 </svelte:head>
-
-<main>
-	<h1>Welcome to Tari Policy Quiz</h1>
-	{#if data.user}
-		<p>Signed in as {data.user.email}</p>
-	{:else}
-		<p>Please sign in to continue.</p>
-	{/if}
-</main>
